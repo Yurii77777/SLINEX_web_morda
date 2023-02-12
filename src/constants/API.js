dc64658c85ae10d2b54bb46d5d459c82e1c0b619
Yurii77777 <yuretshome@gmail.com>
2023-02-12 13:12:35 +0200

    replace base url

--- src/constants/API.js
+++ src/constants/API.js
@@ -1,4 +1,5 @@
 export const API = {
-    BASE_URL: "http://localhost:3080",
+    BASE_URL: "https://slinex-tg-bot.herokuapp.com/",
+    // BASE_URL: "http://localhost:3080",
     GET_PRODUCTS: "/api/get-products",
 };
